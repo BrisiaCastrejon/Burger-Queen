4a2a353532e356e5a182cff8d4ab2e73bb5910f2
BrisiaCastrejon <brissmonse@gmail.com>
2018-10-26 10:49:59 -0500

    Updates

---
+++ precache-manifest.03b32cab99c28c770b3592fd7236b405.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "66d60e76afcc8ebede9f",
+    "url": "/Burger-Queen/static/js/runtime~main.66d60e76.js"
+  },
+  {
+    "revision": "4da0b2481f466fa4676e",
+    "url": "/Burger-Queen/static/js/main.4da0b248.chunk.js"
+  },
+  {
+    "revision": "4ee63fb695c31a77767c",
+    "url": "/Burger-Queen/static/js/1.4ee63fb6.chunk.js"
+  },
+  {
+    "revision": "4da0b2481f466fa4676e",
+    "url": "/Burger-Queen/static/css/main.cf96f7ab.chunk.css"
+  },
+  {
+    "revision": "992a9efe252bdde043b37e5396b0bc72",
+    "url": "/Burger-Queen/index.html"
+  }
+];
